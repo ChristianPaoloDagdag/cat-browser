3ef210992e9c9a63ab5d2db367b923bb34f29ade
ChristianPaoloDagdag <christianpaolodagdag@gmail.com>
2022-11-27 16:53:06 +0800

    fix: put error to the top of the page for SingleCat page

--- src/pages/SingleCat/styles.jsx
+++ src/pages/SingleCat/styles.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   padding 25px; 15px;
 `;
 
